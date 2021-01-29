4daa415ea9a8fbc228166c7fc50f4a2e829f4e04
Adrian Werimo <adrwer7@gmail.com>
2021-01-30 00:06:20 +0300

    Firebase and firestore setup

--- App.js
+++ App.js
@@ -1,6 +1,26 @@
+// @refresh reset
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, LogBox } from 'react-native';
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+import { APIKEY, AUTHDOMAIN, PROJECTID, STORAGEBUCKET, MESSAGINGSENDERID, APPID } from '@env'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: APIKEY,
+  authDomain: AUTHDOMAIN,
+  projectId: PROJECTID,
+  storageBucket: STORAGEBUCKET,
+  messagingSenderId: MESSAGINGSENDERID,
+  appId: APPID
+};
+// Initialize Firebase
+if (firebase.app.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+LogBox.ignoreLogs(['Setting a timer for a long period of time'])
 
 export default function App() {
   return (
