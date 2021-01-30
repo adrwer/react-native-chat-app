e92bae4cdb98b8777023ebf15b202e40669b3320
Adrian Werimo <adrwer7@gmail.com>
2021-01-30 03:49:33 +0300

    Added local user support

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 // @refresh reset
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, LogBox } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, TextInput, View, Button, LogBox } from 'react-native';
 import * as firebase from 'firebase';
 import 'firebase/firestore';
-import { APIKEY, AUTHDOMAIN, PROJECTID, STORAGEBUCKET, MESSAGINGSENDERID, APPID } from '@env'
+import AsyncStorage from '@react-native-community/async-storage';
+import { APIKEY, AUTHDOMAIN, PROJECTID, STORAGEBUCKET, MESSAGINGSENDERID, APPID } from '@env';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -23,9 +24,39 @@ if (firebase.app.length === 0) {
 LogBox.ignoreLogs(['Setting a timer for a long period of time'])
 
 export default function App() {
+  const [user, setUser] = useState(null)
+  const [name, setName] = useState('')
+
+  useEffect(() => {
+    readUser()
+  },[])
+
+  async function readUser() {
+    const user = AsyncStorage.getItem('user')
+    if (user) {
+      setUser(JSON.parse(user))
+    }
+  }
+
+  async function handlePress() {
+    const _id = Math.random().toString(36).substring(7)
+    const user = {_id, name}
+    await AsyncStorage.setItem('user', JSON.stringify(user))
+    setUser(user)
+  }
+
+  if (!user) {
+    return (
+      <View style={styles.container}>
+        <TextInput style={styles.input} placeholder='Enter your name' value={name} onChangeText={setName} />
+        <Button onPress={handlePress} title='Enter the chat' />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>We have a new user</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -37,5 +68,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 30
   },
+  input: {
+    height: 50,
+    width: '100%',
+    borderWidth: 1,
+    padding: 15,
+    marginBottom: 20,
+    borderColor: 'gray'
+  }
 });
