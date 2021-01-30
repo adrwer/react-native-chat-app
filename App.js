7282b7e756106ff4a76a35a4761a41cba66db3be
Adrian Werimo <adrwer7@gmail.com>
2021-01-30 18:03:52 +0300

    Setting up db

--- App.js
+++ App.js
@@ -23,12 +23,27 @@ if (firebase.app.length === 0) {
 
 LogBox.ignoreLogs(['Setting a timer for a long period of time'])
 
+const db = firebase.firestore()
+const chats = db.collection('chats')
+
 export default function App() {
   const [user, setUser] = useState(null)
   const [name, setName] = useState('')
+  const [messages, setMessages] = useState([])
 
   useEffect(() => {
     readUser()
+    const unsubscribe = chats.onSnapshot((querySnapshot) => {
+      const messagesFirestore = querySnapshot
+        .docChanges()
+        .filter(({type}) => type === 'added')
+        .map(({doc}) => {
+          const message = doc.data()
+          return {...message, createdAt: message.createdAt.toDate()}
+        })
+        .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+      setMessages(messagesFirestore)  
+    })
   },[])
 
   async function readUser() {
